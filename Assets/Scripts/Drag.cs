8a249ecaca50151df4502bbb5edd797cc6d52721
MAREK KULA <MAREK.KULA@ing.com>
2023-05-17 08:47:00 +0200

    drop in slot

--- Assets/Scripts/Drag.cs
+++ Assets/Scripts/Drag.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class Drag : MonoBehaviour
 {
+    private bool canDrag;
     private bool dragging;
+    private bool isOnSlot;
 
     private float startPosX;
     private float startPosY;
+
+    private Vector3 startingPosition;
+    private Vector3 slotPosition;
     void Start()
     {
-        
+        startingPosition = transform.position;
+        canDrag = true;
+        isOnSlot = false;
     }
 
     // Update is called once per frame
@@ -36,12 +43,39 @@ public class Drag : MonoBehaviour
             startPosX = mousePos.x - transform.localPosition.x;
             startPosY = mousePos.y - transform.localPosition.y;
 
-            dragging = true;
+            if (canDrag) dragging = true;
         }
     }
 
     private void OnMouseUp()
     {
         dragging = false;
+
+        if (isOnSlot)
+        {
+            transform.position = slotPosition;
+            dragging = false;
+            canDrag = false;
+        } else if (!isOnSlot)
+        {
+            transform.position = startingPosition;
+        }        
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.name);
+        Slot slot;
+        if (collision.TryGetComponent<Slot>(out slot))
+        {
+            
+            if (slot.slotFor == gameObject.name)
+            {
+                slotPosition = collision.transform.position;
+                isOnSlot = true;
+            }
+        }
+    }
+
+
 }
---
+++ Assets/Scripts/Slot.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slot : MonoBehaviour
+{
+    public string slotFor;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
