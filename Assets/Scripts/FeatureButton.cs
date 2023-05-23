d0ec62092e6a4b41ab93b937273881ca7e272869
Marek <markuspoland@gmail.com>
2023-05-23 16:11:16 +0200

    buttons, panel

---
+++ Assets/Scripts/FeatureButton.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeatureButton : MonoBehaviour
+{
+    public Sprite enabledSprite;
+    public Sprite disabledSprite;
+
+    public Feature feature;
+
+    public bool FeatureEnabled { get; set; }
+
+    void Start()
+    {
+        FeatureEnabled = true;
+        GetComponent<SpriteRenderer>().sprite = enabledSprite; 
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (FeatureEnabled)
+            {
+                Disable();
+            } else if (!FeatureEnabled)
+            {
+                Enable();
+            }
+        }
+    }
+
+    
+
+    public void Enable()
+    {
+        FeatureEnabled = true;
+        EnableFeature();
+    }
+
+    public void Disable()
+    {
+        FeatureEnabled = false;
+        DisableFeature();
+    }
+
+    private void DisableFeature()
+    {
+        GetComponent<SpriteRenderer>().sprite = disabledSprite;
+
+        switch (feature)
+        {
+            case Feature.MUSIC:
+                //TODO: Turn off music
+                break;
+            case Feature.SOUNDS:
+                //TODO: Turn off sounds
+                break;
+        }
+    }
+
+    private void EnableFeature()
+    {
+        GetComponent<SpriteRenderer>().sprite = enabledSprite;
+
+        switch (feature)
+        {
+            case Feature.MUSIC:
+                //TODO: Turn on music
+                break;
+            case Feature.SOUNDS:
+                //TODO: Turn on sounds
+                break;
+        }
+    }
+}
